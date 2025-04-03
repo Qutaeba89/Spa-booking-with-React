db9924a3e2c26e1c207cc24c2f88646f7a1ab2a2
Adam Nordgren <adno96@gmail.com>
2025-04-03 22:11:35 +0200

    Added booking form with price calculation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import BookingCalendar from './components/calendar/bookingCalendar'
+import BookingForm from './components/bookingForm/bookingForm';
 
 function App() {
   return(
     <>
        <BookingCalendar />
+       <BookingForm />
     </>
    );
 }
