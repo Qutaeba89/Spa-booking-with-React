a0f5316da9501a1310a420eb12ff6d4569ea5502
markusgusthalin <gusthalin_markus@hotmail.com>
2025-04-04 10:46:02 +0200

    Funktion för att färglägga dagarna baserat på antalet tillgängliga tider

--- src/components/calendar/bookingCalendar.tsx
+++ src/components/calendar/bookingCalendar.tsx
@@ -1,6 +1,21 @@
 import React from 'react'
 import Calendar from 'react-calendar'
 import 'react-calendar/dist/Calendar.css';
+import './bookingCalendar.css'
+
+//Interface för en dag.
+interface Day {
+    date: string;
+    availableTimes: number;
+}
+
+//Mock-data.
+const bookings: Day[] = [
+    {date: '2025-04-03', availableTimes: 0},
+    {date: '2025-04-10', availableTimes: 1},
+    {date: '2025-04-17', availableTimes: 2},
+    {date: '2025-04-24', availableTimes: 3},
+];
 
 function BookingCalendar() {
 
@@ -11,6 +26,35 @@ function BookingCalendar() {
         return false;
 
     }
+
+    function colorDay({ date, view }: { date: Date, view: string }) {
+
+        if (view != 'month') {
+            return;
+        }
+
+        let dateString = date.toISOString().split('T')[0];
+        let booking!: Day;
+
+        for (let i = 0; i < bookings.length; i++) {
+            if (bookings[i].date == dateString) {
+                booking = bookings[i];
+                break;
+            }
+        }
+
+        if (!booking) {
+            return 'greenDay';
+        }
+        if (booking.availableTimes == 0) {
+            return 'redDay';
+        }
+        if (booking.availableTimes < 3) {
+            return 'yellowDay';
+        }
+        return 'greenDay';
+    }
+
 
     function isRedDay() {
 
@@ -24,6 +68,7 @@ function BookingCalendar() {
        <Calendar 
             minDate={new Date()}
             tileDisabled={isMonday}
+            tileClassName={colorDay}
         />
     </>
    );
