d3db4c56ad980c410c772627fcc3958ec3773749
markusgusthalin <gusthalin_markus@hotmail.com>
2025-04-06 20:02:22 +0200

    Lagt till header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import BookingCalendar from './components/calendar/bookingCalendar'
 import BookingForm from './components/bookingForm/bookingForm';
+import './App.css'
 
 function App() {
   return(
-    <>
-       <BookingCalendar />
-       <BookingForm />
+    <>  
+      <header className='header-bar'>
+         <span className='header-title'>Spa med Spa</span>
+         <img src='/candle.gif' className='candle'/>
+      </header>
+      <BookingCalendar />
+      <BookingForm />
     </>
    );
 }
